a8d65d6704c57679315093e272390d70bb8751be
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-11-29 10:57:59 +0900

    firebase_authのサンプル作成

---
+++ firebase_auth_sample/android/app/src/main/kotlin/com/makumaaku/firebase_auth_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.makumaaku.firebase_auth_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ firebase_auth_sample/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth_sample/login_demo_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginDemoPage(),
+    );
+  }
+}
